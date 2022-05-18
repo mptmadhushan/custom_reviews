3eceb59c6218ddeb6ce6ae75cb35fb449e3a3752
mptmadhushan <tharindu.madhushan@axiatadigitallabs.com>
2022-05-18 08:14:18 +0530

    product fixes

--- src/components/ProductsListLevel.js
+++ src/components/ProductsListLevel.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import ProductService from '../services/ProductService';
 import { Link } from 'react-router-dom';
-
 const ProductListLevel = () => {
 	const [ products, setProducts ] = useState([]);
-
-	// useEffect(() => {
-	//   retrieveProducts();
-	// }, ['item']);
 	useEffect(() => {
 		retrieveProducts();
 	}, []);
+  
+  //get products by level
 	const retrieveProducts = () => {
 		console.log('item slug', window.location.pathname.replace('/products/level/', ''));
 		const level = window.location.pathname.replace('/products/level/', '');
@@ -18,11 +15,10 @@ const ProductListLevel = () => {
 		ProductService.getByLevel(data)
 			.then((response) => {
 				const newProducts = response.data.products;
-				console.log('🚀 ~ file:  newProducts', newProducts);
-				const arr = JSON.parse(`${newProducts.replace(/'/g, '"')}`);
 
-				console.log(arr);
-				setProducts(arr);
+				var myArray = newProducts.split("|");				
+				console.log('🚀 api resp -->', myArray);
+				setProducts(myArray);
 			})
 			.catch((e) => {
 				console.log(e);
@@ -36,10 +32,9 @@ const ProductListLevel = () => {
 				const newProducts = response.data.products;
 				console.log('🚀 ~ file:  newProducts', newProducts);
 
-				var arr = JSON.parse(`${newProducts.replace(/'/g, '"')}`);
-
-				console.log(arr);
-				setProducts(arr);
+				var myArray = newProducts.split("|");				
+				console.log('🚀 api resp -->', myArray);
+				setProducts(myArray);
 			})
 			.catch((e) => {
 				console.log(e);
@@ -51,31 +46,16 @@ const ProductListLevel = () => {
 			<div className="col-md-12 text-center">
 				<h4>Products List By Level</h4>
 				<div className="row text-center" style={{marginLeft:'7vw'}}>
-					<Link to={'/products/level/low'} className="nav-link btn-r m-3">
+					<Link onClick={() => refreshList('low')} to={'/products/level/low'} className="nav-link btn-r m-3">
 						Low
-					</Link>{' '}
-					<Link to={'/products/level/Mid'} className="nav-link btn-r m-3">
+					</Link>
+					<Link  onClick={() => refreshList('mid')}  to={'/products/level/Mid'} className="nav-link btn-r m-3">
 						Mid
-					</Link>{' '}
-					<Link to={'/products/level/high'} className="nav-link btn-r m-3">
+					</Link>
+					<Link  onClick={() => refreshList('high')}  to={'/products/level/high'} className="nav-link btn-r m-3">
 						High
 					</Link>
 				</div>
-
-				{/* <div className="row">
-        <button
-          className="m-3 btn btn-sm btn-danger"
-          onClick={refreshList('low')}
-        >
-        low
-        </button> <button
-          className="m-3 btn btn-sm btn-warning"
-          // onClick={refreshList('mid')}
-        >
-          mid
-        </button>
-        
-        </div> */}
 				<ul className="list-group">
 					{products &&
 						products.map((product, index) => (
